2511ddb6a612cbe64e536acead294fee991256e6
johan <johan.muco@magoware.tv>
2022-03-18 06:56:15 +0100

    get location once

--- app/src/main/java/com/example/breweryapp/utils/Utils.kt
+++ app/src/main/java/com/example/breweryapp/utils/Utils.kt
@@ -3,4 +3,7 @@ package com.example.breweryapp.utils
 object Utils {
     const val GET_BREWERIES_BY_CITY = "/breweries"
     const val BASE_URL = "https://api.openbrewerydb.org"
+    const val LATITUDE = "latitude"
+    const val LONGITUDE = "longitude"
+    const val MY_PREFERENCE = "MY_PREFERENCE"
 }
